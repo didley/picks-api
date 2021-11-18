66f55430595a419b4a9b0d5208197fa4f34205b9
Dylan Lamont <dylanlamont3840@gmail.com>
2021-11-18 21:26:28 +1100

    Hardcodes CORS allowed origins to test heroku deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ import cors from "cors";
 mongoose.set("debug", process.env.MONGOOSE_DEBUG === "true" || false);
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 }));
+
+const allowedOrigins = [
+  "https://picks.didley.dev",
+  "https://didley-picks.netlify.app",
+];
+
+app.use(cors({ origin: allowedOrigins, optionsSuccessStatus: 200 }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
